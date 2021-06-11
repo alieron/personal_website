6fa8b8a218e7c3b371b8bcb35fffddf8eb9a17b2
alieron <sebastien.leib1@gmail.com>
2021-06-11 13:25:50 +0800

    Try: 11/06/2021

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Main from '../views/Main.vue'
-import About from '../views/About.vue'
-import Projects from '../views/Projects.vue'
+import Main from '@../views/Main.vue'
+import About from '@../views/About.vue'
+import Projects from '@../views/Projects.vue'
 
 const routes = [
   {
